c44c7d2a1e0544b78a45a3ec6574d70961d6d3db
Arunav <arunavtnt@gmail.com>
2025-12-17 15:34:51 +0530

    Fix application form import/export mismatch

--- wavelaunch-crm/src/app/apply/form/page.tsx
+++ wavelaunch-crm/src/app/apply/form/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ApplicationFormRoot } from '@/modules/application-form'
+import ApplicationFormRoot from '@/modules/application-form'
 
 export default function ApplyFormPage() {
   return <ApplicationFormRoot />
