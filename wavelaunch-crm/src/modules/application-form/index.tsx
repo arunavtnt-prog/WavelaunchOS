f4eb23f5a8137e92d614c4495a38296763e84bbf
Arunav <arunavtnt@gmail.com>
2025-12-17 15:23:45 +0530

    Complete ApplicationFormRoot marker test with proper JSX

--- wavelaunch-crm/src/modules/application-form/index.tsx
+++ wavelaunch-crm/src/modules/application-form/index.tsx
@@ -1,5 +1,11 @@
 "use client"
 
+import React from "react"
+
 export default function ApplicationFormRoot() {
   return (
     <div style={{ padding: 40, fontSize: 32 }}>
+      🚨 NEW APPLICATION FORM MODULE IS RENDERING 🚨
+    </div>
+  )
+}
