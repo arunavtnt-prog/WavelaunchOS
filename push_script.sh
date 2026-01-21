24e40d6b32b5016cbd6b1d6710af4a27b7c84a46
Arunav <arunavtnt@gmail.com>
2026-01-21 11:31:32 +0530

    Cleanup temporary scripts

--- push_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git remote -v
-git status
-git add .
-git commit -m "Implement features"
-git remote add origin https://github.com/arunavtnt-prog/penguin-wavelaunch-os.git || true
-git remote add second-origin https://github.com/arunavtnt-prog/WavelaunchOS.git || true
-git push origin main
-git push second-origin main
